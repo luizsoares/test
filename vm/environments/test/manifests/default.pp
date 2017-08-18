e96cf113de44daab1c12d4eec1c5019921b5c7b8
Luiz Soares <luizsoares@gmail.com>
2017-08-18 00:21:09 -0400

    Separate the postgresql installation from the initial puppet run
    This aligns better with what the exercise asks

--- vm/environments/test/manifests/default.pp
+++ vm/environments/test/manifests/default.pp
@@ -1,13 +1,2 @@
-class {'postgresql::server':}
+class {'datadog_agent':}
 
-postgresql::server::role { 'datadog':
-  password_hash => postgresql_password('datadog', 'datadog')
-}
-
-class {'datadog_agent':
-  api_key => 'da46f1185ebfce0a90279578fba900a2'
-}
-
-class {'datadog_agent::integrations::postgres':
-  password => 'datadog'
-}
